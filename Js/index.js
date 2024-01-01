1af85215a749f86f3fb1a5cb87eae6b36930f609
AmmarAshraf98 <elzaem.ammar12@gmail.com>
2024-01-01 14:49:37 +0200

    repaire url error

--- Js/index.js
+++ Js/index.js
@@ -189,8 +189,12 @@ logBtn.addEventListener("click", function () {
         // set new key for the currentuser in localstorage
         localStorage.setItem("currentUser", i.name);
 
-        // location.href = "https://ammarashraf98.github.io/home.html";
-        window.open("https://ammarashraf98.github.io/pages/home.html");
+        location.href =
+          "https://ammarashraf98.github.io/login-system/home.html";
+
+        // window.open(
+        //   "https://ammarashraf98.github.io/login-system/pages/home.html"
+        // );
         errLogin.innerHTML = ``;
         return;
       } else {
